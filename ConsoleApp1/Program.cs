316b8a64fe1cb6dbd3670911b0f49bb7fe16048c
Florentiene <vilma.staliulionyt@gmail.com>
2022-12-14 03:06:45 +0200

    Add calculator logic

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, sum;
+
+            Console.Write("input a:");
+            x = int.Parse(Console.ReadLine());
+            Console.Write("input b:");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            sum = x + y;
+
+            Console.WriteLine($"Sum is {sum}");
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close ...");
+            Console.ReadKey();
+        }
+    }
+}
